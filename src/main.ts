a5e2b3a49fb71e8f3993670d46e4cba221e633df
akxmai <admin@akamai.uz>
2025-04-23 23:18:51 +0500

    Updated FQDN

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://unduck.akamai.uz?q=%s"
             readonly 
           />
           <button class="copy-button">
